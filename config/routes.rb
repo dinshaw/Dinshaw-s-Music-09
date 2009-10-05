055221ba10eed536fd17f9dfb759bfdf7097b942
Dinshaw Gobhai <info@dinshawdesign.com>
2009-10-05 02:39:05 -0400

    error pages

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action.:format'
   map.connect ':controller/:action/:id.:format'
-  # catch for cms pages
-  map.connect '/:action', :controller => "public"
+
 end
