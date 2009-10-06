8d18b2ec81beeab118feff4c76a77a6d7b0be3d8
Dinshaw Gobhai <info@dinshawdesign.com>
2009-10-06 00:19:49 -0400

    FIX GIGS

--- app/controllers/admin/venues_controller.rb
+++ app/controllers/admin/venues_controller.rb
@@ -1,8 +1,9 @@
 class Admin::VenuesController < AdminController
  admin_assistant_for Venue do |a|
    a.form do |form|
-     form.columns :name, :location, :description
+     form.columns :name, :location, :description, :directions
      form[:description ].text_area_options = {:rows => 5}    
+     form[:directions ].text_area_options = {:rows => 5}         
    end
  end
 end
