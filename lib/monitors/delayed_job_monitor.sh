c78800edadabc3683bfb442799fcc9748434639f
Dinshaw Gobhai <info@dinshawdesign.com>
2009-10-09 10:13:25 -0400

    monitors

--- lib/monitors/delayed_job_monitor.sh
+++ lib/monitors/delayed_job_monitor.sh
@@ -13,12 +13,12 @@ send_mail()
   rm -f $MAIL_BODY
 }
 
-i=`ps aux | grep delayed_job |grep  -v "grep" | wc -l`
+i=`ps | grep delayed_job |grep  -v "grep" | wc -l`
 if [ $i -eq 0 ]
   then
   {
     cd /home/dinshaw1/dinshaw.us/current/
-    /usr/local/bin/ruby /usr/local/bin/ar_sendmail -d --batch-size 4 --delay 60 --environment production
+    /usr/local/bin/ruby script/delayed_job start
     echo "Your Delayed_job was found down at `date '+DATE: %m/%d/%y   TIME:%H:%M:%S'`" >> /home/dinshaw1/dinshaw.us/current/log/delayed_job.log
     echo "A restart was attempted automatically."  >> /home/dinshaw1/dinshaw.us/current/log/delayed_job.log
     send_mail
