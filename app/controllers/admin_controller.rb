060b834653ca978d2fd139daecbc4da4a5a9c4ae
Dinshaw Gobhai <info@dinshawdesign.com>
2009-10-20 10:31:07 -0400

    notification routes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,6 @@ class AdminController < ApplicationController
   end
 
   rescue_from 'Acl9::AccessDenied', :with => :access_denied
-  htpasswd :user=>"dinshaw", :pass=>"Y/bHA8xEf{vTQ6RKDk" unless ['development','test','staging'].include?(RAILS_ENV)
 
   # before_filter :login_required
   layout 'admin'
