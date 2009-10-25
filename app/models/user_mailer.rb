ccc67f49d0a6e6ce37592f36e2371ce8cc07b1c8
Dinshaw Gobhai <info@dinshawdesign.com>
2009-10-25 14:49:49 -0400

    forgot password

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -18,11 +18,9 @@ class UserMailer < ActionMailer::Base
   end
 
   def password_reset_instructions(user)  
-    subject       "Password Reset Instructions"  
-    from          "Binary Logic Notifier "  
-    recipients    user.email  
-    sent_on       Time.now  
-    body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
+    setup_email(user)    
+    @subject    += "Password Reset Instructions"  
+    @body[:edit_password_reset_url] = edit_password_reset_url(user.perishable_token)  
   end  
 
   protected
