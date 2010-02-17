83eb43181d3615ae11170530785a39f496a757c1
Dinshaw Gobhai <info@dinshawdesign.com>
2010-02-17 11:39:29 -0500

    remove track

--- app/controllers/admin/recordings_controller.rb
+++ app/controllers/admin/recordings_controller.rb
@@ -1,15 +1,15 @@
 class Admin::RecordingsController < AdminController
   cache_sweeper :recording_sweeper, :only => [:create, :update, :destroy]
-  
+
   admin_assistant_for Recording do |a|
     a.actions << :destroy
-      a.index do |index|
-        index.columns :track, :song, :recording_session, :in_player_rotation
-      end      
-      a.form do |form|
-        form[:lyrics ].text_area_options = {:rows => 5}
-        form[:notes ].text_area_options = {:rows => 5}      
-        form[:composition_date].date_select_options = {:start_year => 1988,:discard_day => true}
-      end
+    a.index do |index|
+      index.columns :song, :recording_session, :in_player_rotation
+    end      
+    a.form do |form|
+      form[:lyrics ].text_area_options = {:rows => 5}
+      form[:notes ].text_area_options = {:rows => 5}      
+      form[:composition_date].date_select_options = {:start_year => 1988,:discard_day => true}
+    end
   end
 end
--- app/helpers/recording_helper.rb
+++ app/helpers/recording_helper.rb
@@ -2,10 +2,18 @@ module RecordingHelper
   def song_html_for_index(recording)
     link_to recording.song.title, admin_song_path(recording.song)
   end
-  def track_file_name_html_for_index(recording)
-    "render :partial => 'recording', :locals => { :recording => recording }"
-  end
+
   def in_player_rotation_html_for_index(recording)
     is(recording.in_player_rotation)
+  end
+end
+
+class AdminAssistant
+  class PaperclipColumn < Column
+    class IndexView < View
+      def html(record)
+        ''
+      end
+    end
   end
 end
