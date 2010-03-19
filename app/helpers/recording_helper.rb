7b0f80f963f2a1ddc75d06b992019da882b29b92
Dinshaw Gobhai <info@dinshawdesign.com>
2010-03-19 18:14:53 -0400

    asd

--- app/helpers/recording_helper.rb
+++ app/helpers/recording_helper.rb
@@ -6,13 +6,17 @@ module RecordingHelper
   def in_player_rotation_html_for_index(recording)
     is(recording.in_player_rotation)
   end
+
+  def public_html_for_index(recording)
+    is(recording.public)
+  end
 end
 
 class AdminAssistant
   class PaperclipColumn < Column
     class IndexView < View
       def html(record)
-        ''
+        record.track_file_name
       end
     end
   end
